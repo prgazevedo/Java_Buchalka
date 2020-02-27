951adebc25d100a5778d9463c6d5c764fd5c1719
pedroazevedo <prgazevedo@gmail.com>
2020-02-27 13:07:19 +0000

    Update

--- Exercise_Solutions/src/com/Section8/Arrays_Lists_Autoboxing/AutoboxingChallenge/BankInterface.java
+++ Exercise_Solutions/src/com/Section8/Arrays_Lists_Autoboxing/AutoboxingChallenge/BankInterface.java
@@ -4,19 +4,20 @@ import java.util.Scanner;
 
 class BankInterface {
 
-    private Scanner scanner;
+    private static Scanner scanner;
     private Bank bank;
 
     public BankInterface(Bank bank) {
         this.bank = bank;
-        this.scanner = new Scanner(System.in);
         System.out.println("Bank Interface started:");
     }
 
     public void runBankInterface(){
-
-
-        while(processBankOptions());
+        this.scanner = new Scanner(System.in);
+        boolean bExit=true;
+        while(bExit){
+            bExit=processBankOptions();
+        }
     }
 
     public void presentBankOptions(){
@@ -31,27 +32,30 @@ class BankInterface {
 
     public boolean processBankOptions(){
         presentBankOptions();
-        String branchName="";
+        String input;
         if(!scanner.hasNextInt()) {
             System.out.println("Please enter a choice number!");
             return true;
         }
-        switch(scanner.nextInt()){
+        switch(Integer.parseInt(scanner.nextLine())){
             case 1:
                 System.out.println("Selected 1-Add Branch");
-                branchName += inputBranchName();
-                if(bank.addNewBranch(branchName)) System.out.println("Branch added");
+
+                System.out.println("Enter branch name:");
+                input= scanner.nextLine();
+                if(bank.addNewBranch(input)) System.out.println("Branch added");
                 return true;
             case 2:
                 System.out.println("Selected 2-Remove Branch");
-                branchName += inputBranchName();
-                if(bank.removeBranch(branchName)) System.out.println("Branch removed");;
+                 input= scanner.nextLine();
+                if(bank.removeBranch(input)) System.out.println("Branch removed");
+                else System.out.println("Branch not removed. Branch not found.");
                 return true;
             case 3:
                 System.out.println("Selected 3-Branch Options");
-                branchName += inputBranchName();
+                 input= scanner.nextLine();
                 presentBranchOptions();
-                processBranchOptions(branchName);
+                processBranchOptions(input);
                 return true;
             case 4:
                 System.out.println("Selected 4-List Branches and Customers");
@@ -107,13 +111,14 @@ class BankInterface {
         System.out.println("No more transactions to add.");
 
     }
-
+/*
     private String inputBranchName(){
         System.out.println("Enter branch name:");
-        System.out.println("test");
         String branchName= scanner.nextLine();
         return branchName;
     }
+*/
+
     private String getCustomerName(){
         System.out.println("Enter customer name:");
         return scanner.nextLine();
